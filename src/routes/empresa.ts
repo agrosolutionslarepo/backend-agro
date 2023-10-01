f90d5ebbf7a3d97e9869942187b74936d869dc65
WW930\a885187 <ar29238585@enel.com>
2023-10-01 19:56:09 -0300

    agrego controller y route de cosecha

--- src/routes/empresa.ts
+++ src/routes/empresa.ts
@@ -5,9 +5,9 @@ const router = express.Router();
 
 // Rutas para la entidad Empresa
 router.get('/getAllEmpresas', EmpresaController.getAllEmpresas); 
-router.get('/:id', EmpresaController.getEmpresaById);
-router.post('/', EmpresaController.createEmpresa);
-router.put('/:id', EmpresaController.updateEmpresa);
-router.delete('/:id', EmpresaController.deleteEmpresa);
+router.get('/getEmpresaById/:id', EmpresaController.getEmpresaById);
+router.post('/createEmpresa', EmpresaController.createEmpresa);
+router.put('/updateEmpresa/:id', EmpresaController.updateEmpresa);
+router.delete('/deleteEmpresa/:id', EmpresaController.deleteEmpresa);
 
 export default router;
