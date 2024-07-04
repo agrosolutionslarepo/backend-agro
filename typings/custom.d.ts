6e3304f5838ca25e0588b7fb3b3bf7e30e54ef10
kyemanning <paciullitomas@gmail.com>
2024-07-03 21:10:25 -0300

    custRequest fix

--- typings/custom.d.ts
+++ typings/custom.d.ts
@@ -1,10 +1,10 @@
-import {Request} from 'express'
+//import {Request} from 'express'
 
-declare global {
-  namespace Express {
-     interface Request {
-      user?: any
-      users?: any
-    }
-  }
-}
+//declare global {
+ // namespace Express {
+ //    interface Request {
+ //     user?: any
+  //    users?: any
+ ///   }
+ // }
+//}
