3b1b0d3275c459ddbd03c0d293e41c7ac14d09fc
Franco <franco.vicente@davinci.edu.ar>
2025-03-24 17:08:34 -0300

    Se configura .env que no se envia al repo con el connection string

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,12 @@
-// TODO Agregar un archivo con las variables como la contraseña del connection string y ponerlo en el gitignore
-
+require('dotenv').config(); // Cargar variables del .env
 const mongoose = require('mongoose');
 
-const DB_URI =  'mongodb+srv://agrosolutions:OBtl9NFsABOqWXLu@clusteragrosolutions.npywedp.mongodb.net/'
+const DB_URI = process.env.DB_URI; // Leer la URI desde las variables de entorno
+
+if (!DB_URI) {
+    console.error("Falta la variable DB_URI en el archivo .env");
+    process.exit(1); // Terminar la ejecución si no está definida
+}
 
 module.exports = async () => {
     const connect = async () => {
@@ -11,7 +15,7 @@ module.exports = async () => {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             });
-            console.log('Conexion correcta');
+            console.log('Conexión correcta a la base de datos');
         } catch (err) {
             console.error('DB: ERROR', err);
         }
