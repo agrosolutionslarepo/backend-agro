551427cb08bdf08e2ba385c928bde2b35a29e199
Kyemanning <paciullitomas@gmail.com>
2025-06-13 13:01:20 -0300

    Improve error checks for cosecha and semilla controllers

--- src/controllers/semilla.controller.ts
+++ src/controllers/semilla.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { semillaService } from '../services/semilla.service';
+import { HttpError } from '../errors/HttpError';
 
 class SemillaController {
-  public async getAllSemillas(req: Request, res: Response, next: NextFunction): Promise<void> {
+  public async getAllSemillas(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
       const idEmpresa = req.user?.idEmpresa;
+      if (!idEmpresa) {
+        return next(new HttpError(401, 'ID de empresa no encontrado en el token'));
+      }
+
       const semillas = await semillaService.getAllSemillas(idEmpresa);
       res.json(semillas);
     } catch (error) {
@@ -12,9 +21,16 @@ class SemillaController {
     }
   }
 
-  public async getSemillaById(req: Request, res: Response, next: NextFunction): Promise<void> {
+  public async getSemillaById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
       const idEmpresa = req.user?.idEmpresa;
+      if (!idEmpresa) {
+        return next(new HttpError(401, 'ID de empresa no encontrado en el token'));
+      }
       const id = req.params.id;
       const semilla = await semillaService.getSemillaById(id, idEmpresa);
       res.json(semilla);
@@ -23,10 +39,17 @@ class SemillaController {
     }
   }
 
-  public async updateSemilla(req: Request, res: Response, next: NextFunction): Promise<void> {
+  public async updateSemilla(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
       const id = req.params.id;
       const idEmpresa = req.user?.idEmpresa;
+      if (!idEmpresa) {
+        return next(new HttpError(401, 'ID de empresa no encontrado en el token'));
+      }
       const actualizada = await semillaService.updateSemilla(id, req.body, idEmpresa);
       res.json(actualizada);
     } catch (error) {
